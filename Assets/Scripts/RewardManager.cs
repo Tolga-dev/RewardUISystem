604204d15c5654dacbfd7ba8f06cda9ef7c49192
Tolga-dev <orhan.konat.12@gmail.com>
2024-10-23 11:35:40 +0300

    Add examples

---
+++ Assets/Scripts/RewardManager.cs
@@ -0,0 +1,32 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class RewardManager : MonoBehaviour
+{
+    
+    [SerializeField] private TextMeshProUGUI rewardToShow;
+    [SerializeField] private Transform Hand;
+    [SerializeField] private Animator handAnim;
+    void Start()
+    {
+        handAnim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("rewardNo"))
+        {
+            var multiplier = other.gameObject.name;
+
+            rewardToShow.text = (500 * float.Parse(multiplier)).ToString();
+            PlayerPrefs.SetFloat("reward",float.Parse( rewardToShow.text));
+        }
+    }
+
+    public void GetTheReward()
+    {
+        CoinReward.Instance.CountCoins();
+        handAnim.enabled = false;
+    }
+}
